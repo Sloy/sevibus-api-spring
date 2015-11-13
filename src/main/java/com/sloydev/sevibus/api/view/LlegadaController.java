06cc6349316c45859613b1494bce55bd5db16c5e
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-11-13 11:11:03 +0100

    Uses path variables instead of query params

--- src/main/java/com/sloydev/sevibus/api/view/LlegadaController.java
+++ src/main/java/com/sloydev/sevibus/api/view/LlegadaController.java
@@ -3,8 +3,8 @@ package com.sloydev.sevibus.api.view;
 import com.sloydev.sevibus.api.domain.ArrivalTimes;
 import com.sloydev.sevibus.api.domain.ArrivalTimesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,8 +14,8 @@ public class LlegadaController {
     @Autowired
     private ArrivalTimesRepository arrivalTimesRepository;
 
-    @RequestMapping("/llegada")
-    public ArrivalTimes llegada(@RequestParam(value = "linea") String linea, @RequestParam(value = "parada") Integer parada) {
+    @RequestMapping("/llegada/{parada}/{linea}")
+    public ArrivalTimes llegada(@PathVariable(value = "linea") String linea, @PathVariable(value = "parada") Integer parada) {
         return arrivalTimesRepository.getArrivals(parada, linea);
     }
 }
