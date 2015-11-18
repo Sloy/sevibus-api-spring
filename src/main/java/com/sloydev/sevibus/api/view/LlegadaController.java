c7eb7349997bd69233d17cec5d9051c03c6af630
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-11-18 20:57:00 +0100

    Returns a list of times by an array of lines

--- src/main/java/com/sloydev/sevibus/api/view/LlegadaController.java
+++ src/main/java/com/sloydev/sevibus/api/view/LlegadaController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class LlegadaController {
 
@@ -14,8 +17,12 @@ public class LlegadaController {
     @Autowired
     private ArrivalTimesRepository arrivalTimesRepository;
 
-    @RequestMapping("/llegada/{parada}/{linea}")
-    public ArrivalTimes llegada(@PathVariable(value = "linea") String linea, @PathVariable(value = "parada") Integer parada) {
-        return arrivalTimesRepository.getArrivals(parada, linea);
+    @RequestMapping("/llegada/{parada}/{lineas}")
+    public List<ArrivalTimes> llegadas(@PathVariable(value = "lineas") String[] lineas, @PathVariable(value = "parada") Integer parada) {
+        List<ArrivalTimes> results = new ArrayList<>(lineas.length);
+        for (String linea : lineas) {
+            results.add(arrivalTimesRepository.getArrivals(parada, linea));
+        }
+        return results;
     }
 }
