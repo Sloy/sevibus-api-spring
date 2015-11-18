144e01700907f9b0e5410764760d0e28c61ecd55
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-11-18 21:30:44 +0100

    Uses a Query param for multiple lines

--- src/main/java/com/sloydev/sevibus/api/view/LlegadaController.java
+++ src/main/java/com/sloydev/sevibus/api/view/LlegadaController.java
@@ -5,6 +5,7 @@ import com.sloydev.sevibus.api.domain.ArrivalTimesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,8 +18,13 @@ public class LlegadaController {
     @Autowired
     private ArrivalTimesRepository arrivalTimesRepository;
 
-    @RequestMapping("/llegada/{parada}/{lineas}")
-    public List<ArrivalTimes> llegadas(@PathVariable(value = "lineas") String[] lineas, @PathVariable(value = "parada") Integer parada) {
+    @RequestMapping("/llegada/{parada}/{linea}")
+    public ArrivalTimes llegada(@PathVariable(value = "linea") String linea, @PathVariable(value = "parada") Integer parada) {
+        return arrivalTimesRepository.getArrivals(parada, linea);
+    }
+
+    @RequestMapping("/llegada/{parada}/")
+    public List<ArrivalTimes> llegadas(@RequestParam(value = "lineas") String[] lineas, @PathVariable(value = "parada") Integer parada) {
         List<ArrivalTimes> results = new ArrayList<>(lineas.length);
         for (String linea : lineas) {
             results.add(arrivalTimesRepository.getArrivals(parada, linea));
