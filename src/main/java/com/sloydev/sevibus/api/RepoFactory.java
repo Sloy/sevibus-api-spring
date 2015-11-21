f0aa9ccd815be366e2a0539b203063d491c03843
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-11-21 20:25:59 +0100

    Uses @Configuration for autowiring dependencies

--- src/main/java/com/sloydev/sevibus/api/RepoFactory.java
+++ src/main/java/com/sloydev/sevibus/api/RepoFactory.java
@@ -1,17 +1,23 @@
 package com.sloydev.sevibus.api;
 
+import com.sloydev.sevibus.api.data.apptusam.AppTussamApi;
 import com.sloydev.sevibus.api.data.mock.MockArrivalTimesRepository;
 import com.sloydev.sevibus.api.data.tussam.TussamArrivalTimesRepository;
 import com.sloydev.sevibus.api.data.tussam.TussamArrivalsSaxHandler;
 import com.sloydev.sevibus.api.domain.ArrivalTimesRepository;
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+@Configuration
 public class RepoFactory {
 
     private static final boolean mock = false;
 
+    @Bean
     public static ArrivalTimesRepository getArrivalTimesRepository() {
         if (mock) {
             return new MockArrivalTimesRepository();
