3690af73d0ec576d93d733ef7acf477295fb37e0
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-10-08 16:38:37 +0200

    Added Firebase to the project

--- src/main/java/com/sloydev/sevibus/api/MoarConfiguration.java
+++ src/main/java/com/sloydev/sevibus/api/MoarConfiguration.java
@@ -2,25 +2,32 @@ package com.sloydev.sevibus.api;
 
 import com.fewlaps.quitnowcache.QNCache;
 import com.fewlaps.quitnowcache.QNCacheBuilder;
-import com.sloydev.sevibus.api.data.apptusam.AppTussamApi;
-import com.sloydev.sevibus.api.data.apptusam.AppTussamArrivalTimesRepository;
-import com.sloydev.sevibus.api.data.mock.MockArrivalTimesRepository;
-import com.sloydev.sevibus.api.data.tussam.TussamArrivalTimesRepository;
-import com.sloydev.sevibus.api.data.tussam.TussamArrivalsSaxHandler;
-import com.sloydev.sevibus.api.domain.ArrivalTimesRepository;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.util.Base64;
 
 @Configuration
 public class MoarConfiguration {
 
     private static final boolean logRequests = false;
+
+    @Value("${firebase.url}")
+    private String firebaseUrl;
+
+    @Value("${firebase.account}")
+    private String firebaseAccountBase64;
 
     @Bean
     public QNCache cacheManager() {
@@ -45,5 +52,18 @@ public class MoarConfiguration {
             client.interceptors().add(logging);
         }
         return client;
+    }
+
+    @Bean
+    public FirebaseDatabase provideFirebaseDatabase() throws FileNotFoundException {
+        byte[] decodedAccountJson = Base64.getDecoder().decode(firebaseAccountBase64);
+        System.out.println(firebaseAccountBase64);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setServiceAccount(new ByteArrayInputStream(decodedAccountJson))
+                .setDatabaseUrl(firebaseUrl)
+                .build();
+        FirebaseApp.initializeApp(options);
+
+        return FirebaseDatabase.getInstance();
     }
 }
